2bb7a80fcd9ae18daf14270d187509b0033a4b18
David Fasel <davidfasel@gmail.com>
2017-01-30 11:27:38 -0500

    migration to online github

--- functional_test.py
+++ functional_test.py
@@ -19,13 +19,9 @@ class NewVisitorTest(unittest.TestCase):
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
 
-        inputbox = self.browser.find_element_by_id('id_new_item') 
-        self.assertEqual(
-            inputbox.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
 
         # She is invited to enter a to-do item straight away 
+        inputbox = self.browser.find_element_by_id('id_new_item') 
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -38,12 +34,14 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-
+        
+        import time
+        time.sleep(100)
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-        any(row.text == '1: Buy peacock feathers' for row in rows),
-            "New to-do item did not appear in table"
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+                "New to-do item did not appear in table"
         )
 
         # There is still a text box inviting her to add another item. She
--- lists/tests.py
+++ lists/tests.py
@@ -28,4 +28,6 @@ class HomePageTest(TestCase):
         self.assertTrue(html.strip().endswith('</html>'))
 
 
-
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
