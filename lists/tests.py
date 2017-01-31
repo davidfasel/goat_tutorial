57dac5f5ccd06aab63341c44eb5f789e8174d145
David Fasel <daf2139@cumc.columbia.edu>
2017-01-30 19:10:37 -0500

    Only saves items when necessary.  Loading page in browser not fixed

--- lists/tests.py
+++ lists/tests.py
@@ -54,3 +54,11 @@ class HomePageTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, 'The first (ever) list item')
         self.assertEqual(second_saved_item.text, 'Item the second')
+
+    def test_only_saves_items_when_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+
+
+
--- lists/views.py
+++ lists/views.py
@@ -4,12 +4,17 @@ from lists.models import Item
 
 # Create your views here.
 def home_page(request):
-    item = Item()
-    item.text = request.POST.get('item_text', '')
-    item.save()
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        #item.text = request.POST.get('item_text', '')
+        #item.save()
+        #easier way without having to save()
+        Item.objects.create(text = new_item_text)
+    else:
+        new_item_text = ''
 
     return render(request, 'home.html', {
-        'new_item_text': item.text 
+        'new_item_text': new_item_text, 
     })
     
 
