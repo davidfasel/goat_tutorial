b484aa8fbb176c2a999149dbbe19c6058f608e73
David Fasel <daf2139@cumc.columbia.edu>
2017-02-06 18:10:26 -0500

    Fixed unit test at end of Ch 5

--- lists/tests.py
+++ lists/tests.py
@@ -22,11 +22,11 @@ class HomePageTest(TestCase):
         response = self.client.get('/')  
         self.assertTemplateUsed(response, 'home.html')  
         
-        html = response.content.decode('utf8')  
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
+#        html = response.content.decode('utf8')  
+#        self.assertTrue(html.startswith('<html>'))
+#        self.assertIn('<title>To-Do lists</title>', html)
+#        self.assertTrue(html.strip().endswith('</html>'))
+#
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
@@ -35,8 +35,14 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-        self.assertIn('A new list item', response.content.decode())
+#       self.assertIn('A new list item', response.content.decode())
 #       self.assertTemplateUsed(response, 'home.html')  
+
+    def test_redirects_after_POST(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
+
 
     def test_saving_and_retrieving_items(self):
         first_item = Item()
